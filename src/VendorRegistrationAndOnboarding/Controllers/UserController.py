fb2aed8fdef9d5c24b4ee196bb1b1a8e46382dfa
MohibUllahKhanSherwani <mohibkhansherwani@gmail.com>
2026-08-15 09:30:35 +0500

    refactor: Use Register DTO instead of BaseUser in controller

--- src/VendorRegistrationAndOnboarding/Controllers/UserController.py
+++ src/VendorRegistrationAndOnboarding/Controllers/UserController.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from VendorRegistrationAndOnboarding.Services.UserService import UserService
-from VendorRegistrationAndOnboarding.DTOs.UserDTO import BaseUser
+from VendorRegistrationAndOnboarding.DTOs.AuthDTO import Register, Login
 
 router = APIRouter()
 user_service = UserService()
 
 @router.post("/signup")
-async def signup(data: BaseUser):
+async def signup(data: Register):
     return await user_service.create_user(data)
