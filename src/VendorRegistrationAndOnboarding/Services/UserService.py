4a28468f654506e96fbb4e365461f7d245565b2d
MohibUllahKhanSherwani <mohibkhansherwani@gmail.com>
2026-08-15 09:30:47 +0500

    refactor: Use Register DTO instead of BaseUser in service

--- src/VendorRegistrationAndOnboarding/Services/UserService.py
+++ src/VendorRegistrationAndOnboarding/Services/UserService.py
@@ -1,13 +1,14 @@
 from VendorRegistrationAndOnboarding.Repositories.UserRepository import UserRepository
 from VendorRegistrationAndOnboarding.utils.utilities import hash_password, verify_password
 from VendorRegistrationAndOnboarding.DTOs.UserDTO import BaseUser
+from VendorRegistrationAndOnboarding.DTOs.AuthDTO import Register
 import uuid
 
 class UserService:
     def __init__(self):
         self.user_repository = UserRepository()
 
-    async def create_user(self, data: BaseUser):
+    async def create_user(self, data: Register):
         user_id = str(uuid.uuid4())
         password = hash_password(data.Password)
         data.Email = data.Email.lower().strip()
@@ -16,11 +17,13 @@ class UserService:
         existing_user = await self.user_repository.get_user_by_email(data.Email)
         if existing_user:
                 return {"success":False,"message":f"User with email{data.Email} already exists."}
-    
+
         user_data = {
             "_id": user_id,
             "Email": data.Email,
-            "UserType": data.UserType, # Will change it once to Admin to create one admin user and then will change it to Vendor
+            "FirstName": data.FirstName,
+            "LastName": data.LastName,
+            "UserType": "Vendor", # Will change it once to Admin to create one admin user and then will change it to Vendor
             "Password": password,
         }
         user_id = await self.user_repository.create_user(user_data)
